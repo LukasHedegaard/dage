052d040e2e92432cba9bd35f96cc7c0a615f2a5b
Lukas Hedegaard <lhj@biir.dk>
2019-10-10 14:25:24 +0200

    update run with keras model. basic training runs

--- run.py
+++ run.py
@@ -1,76 +1,63 @@
 from utils.parse_args import parse_args
 import utils.dataset_gen as dsgen
-from models import ModelTrainer, ClassicModel, CCSAModel, DSNEModel
+import models
+import tensorflow as tf
+keras = tf.compat.v2.keras
 
 def main(args):
 
-    n_classes = len(dsgen.office31_class_names())
+    # data
+    INPUT_SHAPE = (224, 224, 3)
+    OUTPUT_SHAPE = len(dsgen.office31_class_names())
+
+    ds = dsgen.office31_datasets( source_name=args.source, target_name=args.target, img_size=INPUT_SHAPE[:2], seed=args.seed)
+
+    test_ds = [ ds['target']['test'] ]
+
+    train_ds = {
+        'tune_source': lambda: [ ds['source']['full']],
+        'tune_both'  : lambda: [ ds['source']['full'], ds['target']['train'] ],
+        'ccsa'       : lambda: [ dsgen.da_combi_dataset(source_ds=ds['source']['train']['ds'], 
+                                                        target_ds=ds['target']['train']['ds'], 
+                                                        ratio=args.ratio, 
+                                                        shuffle_buffer_size=args.shuffle_buffer_size) ],
+        'dsne'       : lambda: [ dsgen.da_combi_dataset(source_ds=ds['source']['train']['ds'], 
+                                                        target_ds=ds['target']['train']['ds'], 
+                                                        ratio=args.ratio, 
+                                                        shuffle_buffer_size=args.shuffle_buffer_size) ],
+    }[args.method]()
+
+    test_ds  = list(map(lambda d: ( dsgen.prep_test(dataset=d['ds'], batch_size=args.batch_size) , d['size']), test_ds  ))
+    train_ds = list(map(lambda d: ( dsgen.prep_train(dataset=d['ds'], batch_size=args.batch_size) , d['size']), train_ds ))
 
     # model
-    if args.method == 'tune_source':
-        model = ClassicModel(output_dim=n_classes)
-    elif args.method == 'tune_both':
-        model = ClassicModel(output_dim=n_classes)
-    elif args.method == 'ccsa':
-        model = CCSAModel(output_dim=n_classes)
-    elif args.method == 'dsne':
-        model = DSNEModel(output_dim=n_classes)
-    else:
-        raise NotImplementedError
+    model_base = {
+        'vgg16'      : lambda: keras.applications.vgg16.VGG16      (input_shape=INPUT_SHAPE, include_top=False, weights='imagenet'),
+        'resnet50'   : lambda: keras.applications.resnet50.ResNet50(input_shape=INPUT_SHAPE, include_top=False, weights='imagenet'),
+    }[args.model_base]()
 
-    # data
-    ds = dsgen.office31_datasets(
-        source_name=args.source,
-        target_name=args.target,
-        img_size=model.input_dim,
-        seed=args.seed,
-    )
-    if args.method == 'tune_source':
-        train_ds = [
-            ds['source']['full']
-        ]
-    elif args.method == 'tune_both':
-        train_ds = [
-            ds['source']['full'],
-            ds['target']['train']
-        ]
-    elif args.method in ['ccsa', 'dsne']:
-        train_ds = [ 
-            dsgen.da_combi_dataset(
-                ds['source']['train'], 
-                ds['target']['train'], 
-                ratio=args.ratio,
-                shuffle_buffer_size=args.shuffle_buffer_size
-            )
-        ]
-    test_ds = [ds['target']['test']]
+    model, loss = {
+        'tune_source': lambda: ( models.classic(model_base, output_shape=OUTPUT_SHAPE), keras.losses.categorical_crossentropy ),
+        'tune_both'  : lambda: ( models.classic(model_base, output_shape=OUTPUT_SHAPE), keras.losses.categorical_crossentropy ),
+        # 'ccsa'       : lambda: models.CCSAModel(output_dim=OUTPUT_SHAPE),
+        # 'dsne'       : lambda: models.DSNEModel(output_dim=OUTPUT_SHAPE),
+    }[args.method]()
 
-    # model trainer
-    model_trainer = ModelTrainer(
-        model=model,
-        learning_rate=args.learning_rate,
-        output_dir=''
-    )
+    optimizer = {
+        'sgd' : lambda: keras.optimizers.SGD (learning_rate=args.learning_rate, momentum=0.0, nesterov=False),
+        'adam': lambda: keras.optimizers.Adam(learning_rate=args.learning_rate, beta_1=0.9, beta_2=0.999, amsgrad=False)
+    }[args.optimizer]()
+
+    model.compile(loss=loss, optimizer=optimizer, metrics=['accuracy'])
 
     # perform training and test
     if 'train' in args.mode:
-        for d in train_ds:
-            model_trainer.train(
-                dsgen.prep_for_training( 
-                    dataset=d, 
-                    batch_size=args.batch_size,
-                    shuffle_buffer_size=args.shuffle_buffer_size
-                )
-            )
+        for x, s in train_ds:
+            model.fit( x=x, epochs=args.epochs, steps_per_epoch=s//args.batch_size, validation_split=0.0, verbose=1 )
 
     if 'test' in args.mode:
-        for d in test_ds:
-            model_trainer.test(
-                dsgen.prep_for_test( 
-                    dataset=d, 
-                    batch_size=args.batch_size,
-                )
-            )
+        for x, s in test_ds:
+            model.evaluate(x, steps=s//args.batch_size, verbose=0)
 
 
 if __name__ == '__main__':
