260619c0ae32872dcd1e2f975b0b88d56cf18ed4
Lukas Hedegaard <lhj@biir.dk>
2019-10-11 09:26:48 +0200

    update download_office_datasets with correct dir

--- scripts/download_office_datasets.sh
+++ scripts/download_office_datasets.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
 cd datasets
 
-# Download the MNIST
+if [ ! -d "datasets" ]; then
+    mkdir "datasets"
+fi
+
 if [ ! -d "Office31" ]; then
-    mkdir "Office31"
+    mkdir "datasets/Office31"
 fi
 
 wget --no-check-certificate -O tmp.tar.gz "https://drive.google.com/uc?export=download&id=0B4IapRTv9pJ1WGZVd1VDMmhwdlE"
-tar -xvzf tmp.tar.gz -C Office31
+tar -xvzf tmp.tar.gz -C datasets/Office31
 rm tmp.tar.gz
