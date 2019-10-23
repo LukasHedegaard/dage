6b8d6cfeb94011fa1322030ad69637ef7d526455
Lukas Hedegaard <lhj@biir.dk>
2019-10-23 16:01:51 +0200

    remove unnecessary import, fix warning

--- utils/gpu.py
+++ utils/gpu.py
@@ -1,13 +1,11 @@
 import tensorflow as tf
 import os
-from tensorflow.compat.v1 import ConfigProto
-from tensorflow.compat.v1 import InteractiveSession
 
 def setup_gpu(gpu_id, verbose=True):
     os.environ["CUDA_VISIBLE_DEVICES"]=str(gpu_id)
     if verbose:
         print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 
-    config = ConfigProto()
+    config = tf.ConfigProto()
     config.gpu_options.allow_growth = True
-    session = InteractiveSession(config=config)
+    session = tf.InteractiveSession(config=config)
