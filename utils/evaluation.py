da99c702948b10eefce94b349849dcb76864c9a9
Lukas Hedegaard <lhj@biir.dk>
2019-10-25 12:25:43 +0200

    update evaluation, add eval for two-stream model

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -2,9 +2,35 @@ from pathlib import Path
 from sklearn.metrics import classification_report
 import json
 import tensorflow as tf
+tf.compat.v1.enable_eager_execution() 
+from math import ceil
 keras = tf.compat.v2.keras
 Dataset = tf.compat.v2.data.Dataset
 Model = tf.compat.v2.keras.Model
+
+def evaluate_da_pair(
+    model:Model, 
+    test_dataset:Dataset, 
+    test_size:int, 
+    batch_size:int,  
+    # pred_file_path:Path,
+    report_path:Path,
+    verbose:int,
+    target_names=None,
+):
+    return evaluate(
+        model, 
+        test_dataset, 
+        test_size, 
+        batch_size, 
+        # pred_file_path,
+        report_path,
+        verbose,
+        target_names,
+        get_data_and_lbls=lambda batch: (batch[0], batch[1]['preds'] ),
+        get_preds=lambda batch: batch[0],
+    )
+    
 
 def evaluate(
     model:Model, 
@@ -15,25 +41,24 @@ def evaluate(
     report_path:Path,
     verbose:int,
     target_names=None,
+    get_data_and_lbls=lambda x: x,
+    get_preds=lambda x: x,
 ):
     y_true, y_pred = [], []
-    steps = test_size//batch_size
+    steps = ceil(test_size/batch_size)
 
     if verbose:
         print('Performing predictions on test set')
 
-    tf_session = tf.compat.v1.keras.backend.get_session()
-    test_iter = tf.compat.v1.data.make_one_shot_iterator(test_dataset)
-    next_elem = test_iter.get_next()
     progbar = keras.utils.Progbar(steps)
-    for _ in range(steps):
-        result = tf_session.run([next_elem])
-        image_batch, label_batch = result[0]
-        pred_prob_batch = model.predict(image_batch, steps=1, verbose=0)
-        y_pred.extend(pred_prob_batch.argmax(axis=1))
-        y_true.extend(label_batch.argmax(axis=1))
-        if verbose:
-            progbar.add(1)
+    ds_iter = iter(test_dataset)
+    for batch in ds_iter:
+        data, lbls = get_data_and_lbls(batch)
+        pred_probs = model.predict_on_batch(data)
+        pred_probs = get_preds(pred_probs)
+        y_pred.extend(pred_probs.argmax(axis=1))
+        y_true.extend(lbls.numpy().argmax(axis=1))
+        progbar.add(1)
 
     if verbose:
         print('Creating classification report at {}'.format(report_path))    
