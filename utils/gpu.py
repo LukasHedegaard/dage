4d430e91424d7061566516b8a5fa8d802688f513
Lukas Hedegaard Jensen <lhj@monster.st.client.au.dk>
2019-11-01 12:43:39 +0000

    update gpu setup to work with tf.v2

--- utils/gpu.py
+++ utils/gpu.py
@@ -2,10 +2,17 @@ import tensorflow as tf
 import os
 
 def setup_gpu(gpu_id, verbose=True):
-    os.environ["CUDA_VISIBLE_DEVICES"]=str(gpu_id)
-    if verbose:
-        print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
-
-    config = tf.ConfigProto()
-    config.gpu_options.allow_growth = True
-    session = tf.InteractiveSession(config=config)
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            gpu_ids = [int(s) for s in gpu_id.split(',')]
+            gpus = [gpus[i] for i in gpu_ids]
+            tf.config.experimental.set_visible_devices(gpus, 'GPU')
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+            logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+            print("Found {} Physical GPUs, {} Logical GPU".format(len(gpus),len(logical_gpus)))
+            print("Using GPU {}".format(gpu_ids))
+        except RuntimeError as e:
+            # Visible devices must be set before GPUs have been initialized
+            print(e)
