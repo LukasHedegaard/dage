c49f58209a84f6e4a14fd34b965029ceead35426
Lukas Hedegaard <lhj@biir.dk>
2019-11-20 12:03:53 +0100

    make dynamic batch_size read in losses

--- losses/dsne.py
+++ losses/dsne.py
@@ -15,12 +15,8 @@ from utils.dataset_gen import DTYPE
 
 
 def dnse_loss(
-    batch_size = 16,
-    embed_size = 128,
     margin=1
 ):
-    batch_size_target = batch_size_source = batch_size
-
     def loss(y_true, y_pred):
         ''' Tensorflow implementation of d-SNE loss. 
             Original Mxnet implementation found at https://github.com/aws-samples/d-SNE.
@@ -32,15 +28,18 @@ def dnse_loss(
         ys = tf.argmax(tf.cast(y_true[:,0], dtype=tf.int32), axis=1)
         yt = tf.argmax(tf.cast(y_true[:,1], dtype=tf.int32), axis=1)
 
+        batch_size = tf.shape(ys)[0]
+        embed_size = tf.shape(xs)[1]
+
         # The original implementation provided an optional feature-normalisation (L2) here. We'll skip it
 
-        xs_rpt = tf.broadcast_to(tf.expand_dims(xs, axis=0), shape=(batch_size_target, batch_size_source, embed_size))
-        xt_rpt = tf.broadcast_to(tf.expand_dims(xt, axis=1), shape=(batch_size_target, batch_size_source, embed_size))
+        xs_rpt = tf.broadcast_to(tf.expand_dims(xs, axis=0), shape=(batch_size, batch_size, embed_size))
+        xt_rpt = tf.broadcast_to(tf.expand_dims(xt, axis=1), shape=(batch_size, batch_size, embed_size))
 
         dists = tf.reduce_sum(tf.square(xt_rpt - xs_rpt), axis=2)
 
-        yt_rpt = tf.broadcast_to(tf.expand_dims(yt, axis=1), shape=(batch_size_target, batch_size_source))
-        ys_rpt = tf.broadcast_to(tf.expand_dims(ys, axis=0), shape=(batch_size_target, batch_size_source))
+        yt_rpt = tf.broadcast_to(tf.expand_dims(yt, axis=1), shape=(batch_size, batch_size))
+        ys_rpt = tf.broadcast_to(tf.expand_dims(ys, axis=0), shape=(batch_size, batch_size))
 
         y_same = tf.equal(yt_rpt, ys_rpt)
         y_diff = tf.not_equal(yt_rpt, ys_rpt)
@@ -49,7 +48,7 @@ def dnse_loss(
         inter_cls_dists = tf.multiply(dists, tf.cast(y_diff, dtype=DTYPE))
 
         max_dists = tf.reduce_max(dists, axis=1, keepdims=True)
-        max_dists = tf.broadcast_to(max_dists, shape=(batch_size_target, batch_size_source))
+        max_dists = tf.broadcast_to(max_dists, shape=(batch_size, batch_size))
         revised_inter_cls_dists = tf.where(y_same, max_dists, inter_cls_dists)
 
         max_intra_cls_dist = tf.reduce_max(intra_cls_dists, axis=1)
