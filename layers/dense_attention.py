fix bug when using softmax in dense-attention

layers/dense_attention.py
@@ -70,6 +70,7 @@ class DenseAttention(Layer):
             dtype=self.dtype,
             trainable=True
         )
+
         if self.use_bias:
             self.bias = self.add_weight(
                 'bias',
@@ -82,6 +83,16 @@ class DenseAttention(Layer):
             )
         else:
             self.bias = None
+
+        if self.omit_intra_domain:
+            tile_size = [self.batch_size//2, self.batch_size//2, self.classes]
+            zeros = tf.zeros(tile_size, dtype=tf.bool)
+            ones = tf.ones(tile_size, dtype=tf.bool)
+            self.mask = tf.concat([tf.concat([zeros, ones], axis=0),
+                                   tf.concat([ones, zeros], axis=0)], axis=1 )
+        else:
+            self.mask = None
+
         self.built = True
 
     def call(self, inputs, labels):
@@ -113,22 +124,16 @@ class DenseAttention(Layer):
         W = tf.equal(yTe, eTy) if not self.inverse else tf.not_equal(yTe, eTy)
 
         if self.omit_intra_domain:
-            tile_size = [self.batch_size//2, self.batch_size//2, self.classes]
-            zeros = tf.zeros(tile_size, dtype=tf.bool)
-            ones = tf.ones(tile_size, dtype=tf.bool)
-            mask = tf.concat([tf.concat([zeros, ones], axis=0),
-                              tf.concat([ones, zeros], axis=0)], axis=1 )
-            W = tf.logical_and(W, mask)
+            W = tf.logical_and(W, self.mask)
 
         A = tf.where(W, xBBx, tf.zeros_like(xBBx)) # mask using W
         
         if self.activation is not None:
-            A = tf.reshape(A,[self.classes, self.batch_size*self.batch_size])
-            A = self.activation(A) # pylint: disable=not-callable
-            A = tf.reshape(A,[self.batch_size, self.batch_size, self.classes])
+            A = self.activation(A) + self.activation(tf.transpose(A, perm=[1,0,2]))
             A = tf.where(W, A, tf.zeros_like(A)) # mask again to remove spill-over from softmax
             
         outputs = tf.reduce_sum(A, axis=2) #reduce along the channel axis
+
         return outputs
 
     def compute_output_shape(self, input_shape):
@@ -142,6 +147,7 @@ class DenseAttention(Layer):
         config = {
             'classes': self.classes,
             'activation': activations.serialize(self.activation),
+            'omit_intra_domain': self.omit_intra_domain,
             'use_bias': self.use_bias,
             'kernel_initializer': initializers.serialize(self.kernel_initializer),
             'bias_initializer': initializers.serialize(self.bias_initializer),

losses/dage.py
@@ -300,31 +300,25 @@ def dage_attention_loss(
         batch_size = tf.shape(xs)[0]
 
         # construct loss for bad attention
-        W, Wp = make_weights(xs, xt, ys, yt, batch_size)
+        # W, Wp = make_weights(xs, xt, ys, yt, batch_size)
 
         # construct Weight matrix
-        WA = tf.multiply(W, A)
-        WpA = tf.multiply(Wp, Ap)
+        W = A #tf.multiply(W, A)
+        Wp = Ap #tf.multiply(Wp, Ap)
 
         # construct Degree matrix
-        D  = tf.linalg.diag(tf.reduce_sum(WA,  axis=1)) 
-        Dp = tf.linalg.diag(tf.reduce_sum(WpA, axis=1))
+        D  = tf.linalg.diag(tf.reduce_sum(W,  axis=1)) 
+        Dp = tf.linalg.diag(tf.reduce_sum(Wp, axis=1))
 
         # construct Graph Laplacian
-        L  = tf.subtract(D, WA)
-        Lp = tf.subtract(Dp, WpA)
+        L  = tf.subtract(D, W)
+        Lp = tf.subtract(Dp, Wp)
         
         # construct loss
         θϕLϕθ  = tf.matmul(θϕ, tf.matmul(L,  θϕ, transpose_b=True))
         θϕLpϕθ = tf.matmul(θϕ, tf.matmul(Lp, θϕ, transpose_b=True))
 
-        ge_loss = tf.linalg.trace(θϕLϕθ) / tf.linalg.trace(θϕLpϕθ)
-
-        ones = tf.ones_like(W)
-        A_loss  = tf.norm(tf.multiply(ones-W, A), ord='euclidean')
-        Ap_loss = tf.norm(tf.multiply(ones-Wp, Ap), ord='euclidean')
-
-        loss = ge_loss #+ 0.1*(A_loss + Ap_loss)
+        loss = tf.linalg.trace(θϕLϕθ) / tf.linalg.trace(θϕLpϕθ)
 
         return loss
     return loss_fn

models/two_stream_pair_embeds_attention_mid_classwise.py
@@ -70,7 +70,7 @@ def model(
         inverse=True
     )(concat_out, concat_lbl)
 
-    model = keras.models.Model(inputs=[in_src, in_tgt, lbl_src, lbl_tgt], outputs=[preds_src, preds_tgt, att_out, att_p_out])
+    model = keras.models.Model(inputs=[in_src, in_tgt, lbl_src, lbl_tgt], outputs=[mid_out_src, mid_out_tgt, preds_src, preds_tgt, att_out, att_p_out])
     model_test = keras.models.Model(inputs=[in_tgt], outputs=[preds_tgt])
 
     # Add losses 
@@ -83,7 +83,7 @@ def model(
     dage_loss_weight   = tf.cast(loss_alpha, dtype=DTYPE)
 
     model.add_loss(tf.scalar_mul(dage_loss_weight,   dage_loss  ))
-    model.add_loss(tf.scalar_mul(ce_loss_weight_src, ce_loss_src))
+    model.add_loss(tf.scalar_mul(ce_loss_weight_src, ce_loss_src)) 
     model.add_loss(tf.scalar_mul(ce_loss_weight_tgt, ce_loss_tgt))
 
     # Add metrics
@@ -126,3 +126,65 @@ def train(
         callbacks=callbacks,
         verbose=verbose,
     )
+
+def train_flipping(
+    model, 
+    datasource, 
+    datasource_size, 
+    epochs, 
+    batch_size, 
+    callbacks, 
+    verbose=1, 
+    val_datasource=None, 
+    val_datasource_size=None 
+):
+    validation_steps = val_datasource_size//batch_size if val_datasource_size else None
+    steps_per_epoch = datasource_size//batch_size
+
+    if not val_datasource_size:
+        val_datasource = None
+        validation_steps = None
+
+    train_iter = iter(datasource)
+    
+    for e in range(1,epochs+1):
+        if verbose:
+            print('Epoch {}/{}.'.format(e, epochs))
+
+        for step in range(steps_per_epoch):
+            batch = next(train_iter)
+
+            source_loss = model.train_on_batch(batch)
+
+            target_loss = model.train_on_batch({
+                'input_source': batch['input_target'], 
+                'input_target': batch['input_source'],
+                'label_source': batch['label_target'],
+                'label_target': batch['label_source'],
+            })
+
+            if step % 5 == 0 and verbose:
+                print(' Step {}/{}'.format(step, steps_per_epoch))
+                print('  Source Pass:  {}'.format(
+                    '  '.join(['{} {:0.4f}'.format(model.metrics_names[i], source_loss[i]) for i in range(len(source_loss))])
+                ))
+                print('  Target Pass:  {}'.format(
+                    '  '.join(['{} {:0.4f}'.format(model.metrics_names[i], target_loss[i]) for i in range(len(target_loss))])
+                ))
+
+        val_iter = iter(val_datasource)
+        val_loss = []
+        for step in range(validation_steps):
+            batch = next(val_iter)
+            val_loss.append(model.test_on_batch(batch))
+
+        val_loss_avg = reduce(
+            lambda n, o: [n[i]+o[i] for i in range(len(val_loss))],
+            val_loss, 
+            [0 for _ in val_loss[0]]
+        )
+
+        if verbose:
+            print('  Validation:  {}'.format(
+                '  '.join(['{} {:0.4f}'.format(model.metrics_names[i], val_loss_avg[i]) for i in range(len(val_loss_avg))])
+            ))

utils/callbacks.py
@@ -1,6 +1,19 @@
 import tensorflow as tf
 keras = tf.compat.v2.keras
 
+class TerminateOnNegMetric(keras.callbacks.Callback):
+    def __init__(self, metric='aux_loss'):
+        super(TerminateOnNegMetric, self).__init__()
+        self.metric = metric
+
+    def on_batch_end(self, batch, logs=None):
+        logs = logs or {}
+        metric = logs.get(self.metric)
+        if metric is not None:
+            if metric < -0.1:
+                print('Batch %d: Invalid metric, terminating training\n' % (batch))
+                self.model.stop_training = True
+
 def checkpoint(checkpoints_dir, monitor='loss',verbose=True):
     return keras.callbacks.ModelCheckpoint(
         filepath=str(checkpoints_dir / 'cp-best.ckpt'),
@@ -42,5 +55,6 @@ def all(checkpoints_dir, tensorboard_dir, monitor='loss', verbose=True):
         checkpoint(checkpoints_dir, monitor, verbose), 
         reduce_lr(monitor, verbose), 
         early_stop(monitor, verbose), 
-        tensorboard(tensorboard_dir)
+        tensorboard(tensorboard_dir),
+        TerminateOnNegMetric(),
     ]