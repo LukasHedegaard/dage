1b831014fc3a7309494261d055745a8c680448ff
Lukas Hedegaard <lhj@biir.dk>
2020-02-07 15:13:02 +0100

    updated hypersearch

--- hypersearch.py
+++ hypersearch.py
@@ -23,9 +23,6 @@ def parse_args():
     storage.add_argument('--id', type=str, default='', help='Identifyer for the hypersearch')
     storage.add_argument('--from_checkpoint', type=str, default='', help='Path to directory containing checkpoint to continue search from')
     storage.add_argument('--verbose', type=int, default=1, help='Whether to print status outputs')
-    # storage.add_argument('--gpu_ids', type=str, default='', help='IDs of the GPUs to use')
-    # storage.add_argument('--source', type=str, default='W', help='Source dataset')
-    # storage.add_argument('--target', type=str, default='A', help='Target dataset')
 
     search = parser.add_argument_group(description='Search')
     search.add_argument('--n_random_starts', type=int, default=10, help='Number of random starts')
@@ -33,28 +30,6 @@ def parse_args():
     search.add_argument('--acq_func', type=str, default='EI', help='Acquisition function')
     search.add_argument('--noise', type=float, default=None, help='Expected noise level in optimization')
     search.add_argument('--seed', type=int, default=None, help='Seed for the random search')
-
-    param = parser.add_argument_group(description='Hyper parameters')
-    param.add_argument('--lr_min', type=float, default=1e-8, help='Learning rate min')
-    param.add_argument('--lr_max', type=float, default=1e-3, help='Learning rate max')
-    param.add_argument('--inv_mom_min', type=float, default=0.01, help='(1-param), i.e. momentum max')
-    param.add_argument('--inv_mom_max', type=float, default=0.5, help='(1-param), i.e. momentum min')
-    param.add_argument('--lr_decay_min', type=float, default=1e-7, help='Learning rate decay min (relative to number of epochs)')
-    param.add_argument('--lr_decay_max', type=float, default=1e-2, help='Learning rate decay max (relative to number of epochs)')
-    param.add_argument('--dropout_min', type=float, default=0.1, help='Dropout min')
-    param.add_argument('--dropout_max', type=float, default=0.8, help='Dropout max')
-    param.add_argument('--l2_min', type=float, default=1e-7, help='L2 weight decay min')
-    param.add_argument('--l2_max', type=float, default=1e-3, help='L2 weight decay max')
-    param.add_argument('--alpha_min', type=float, default=0.1, help='Relative weighting of domain adaptation loss vs cross entropies')
-    param.add_argument('--alpha_max', type=float, default=0.99, help='Relative weighting of domain adaptation loss vs cross entropies')
-    param.add_argument('--ce_ratio_min', type=float, default=0, help='Source-target cross entropy weighting ratio min')
-    param.add_argument('--ce_ratio_max', type=float, default=1, help='Source-target cross entropy weighting ratio max')
-    param.add_argument('--loss_param_1_min', type=int, default=1, help='Loss parameter 1 minimum value')
-    param.add_argument('--loss_param_1_max', type=int, default=3, help='Loss parameter 1 maximum value')
-    param.add_argument('--loss_param_2_min', type=int, default=8, help='Loss parameter 2 minimum value')
-    param.add_argument('--loss_param_2_max', type=int, default=128, help='Loss parameter 2 maximum value')
-    param.add_argument('--num_unfrozen_min', type=int, default=0, help='Number of unfrozen base layer minimum')
-    param.add_argument('--num_unfrozen_max', type=int, default=16, help='Number of unfrozen base layer maximum')
 
     return parser.parse_args()
 
@@ -73,11 +48,14 @@ def run(args):
         pprint(args)
 
     acc = nn([
-        "--gpu_id",                 "3", #gpu_ids,
-        "--source",                 "W", #source,
-        "--target",                 "A", #target,
-        "--from_weights",           "./runs/tune_source/vgg16_aug_ft_best/WA/checkpoints/cp-best.ckpt",
-        "--num_unfrozen_base_layers", str(args['num_unfrozen']),
+        "--gpu_id",                 "0", #gpu_ids,
+        "--source",                 "mnist", #source,
+        "--target",                 "usps", #target,
+        "--num_source_samples_per_class", "200",
+        "--num_target_samples_per_class", "3",
+        "--num_val_samples_per_class", "10",
+        # "--from_weights",           "./runs/tune_source/vgg16_aug_ft_best/WA/checkpoints/cp-best.ckpt",
+        # "--num_unfrozen_base_layers", str(args['num_unfrozen']),
         # "--seed",                   str(np.random.randint(1000)),
         "--l2",                     str(args['l2']),
         "--dropout",                str(args['dropout']),
@@ -86,32 +64,32 @@ def run(args):
         "--learning_rate",          str(args['lr']),
         "--learning_rate_decay",    str(args['lr_decay']),
         "--momentum",               str(1-args['inv_mom']),
-        "--experiment_id",          "optimizer", #"optimizer{}".format(run_id),
-        "--batch_norm",             "1",
+        "--experiment_id",          "optimizer", 
+        "--batch_norm",             str(args['bn']),
         "--optimizer",              "adam",
-        "--batch_size",             "16",
-        "--epochs",                 "30", 
-        "--augment",                "0",
-        "--model_base",             "vgg16",
-        "--features",               "images",
+        "--batch_size",             str(args['bs']),
+        "--epochs",                 "50", 
+        "--augment",                str(args['augment']),
         "--architecture",           "two_stream_pair_embeds",
-        "--method",                 "dsne",
-        # "--connection_type",                    "SOURCE_TARGET",
-        # "--weight_type",                        "INDICATOR",
-        # "--connection_filter_type",             "KNN",
-        # "--penalty_connection_filter_type",     "KSD",
-        "--connection_filter_param",            str(args['loss_param_1']),
+        "--model_base",             "conv2",
+        "--dense_size",             "120",
+        "--embed_size",             "84",
+        # "--features",               "images",
+        "--method",                 "dage",
+        "--connection_type",                    "SOURCE_TARGET",
+        "--weight_type",                        "INDICATOR",
+        "--connection_filter_type",             "ALL",
+        "--penalty_connection_filter_type",     "ALL",
+        # "--connection_filter_param",            str(args['loss_param_1']),
         # "--penalty_connection_filter_param",    str(args['loss_param_2']),
         "--mode",                   "train_and_test",
         "--training_regimen",       "batch_repeat",
-        "--batch_repeats",          "1", #str(args['batch_repeats']),
-        "--augment",                "1",
+        "--batch_repeats",          "2", #str(args['batch_repeats']),
+        "--augment",                "0",
         # "--timestamp",              "",
         # "--test_as_val",            "0",
-        # "--embed_size",             "",
-        # "--dense_size",             "",
-        "--ratio",                  "1", #str(args['data_ratio']),
-        # "--shuffle_buffer_size",    "",
+        "--ratio",                  "3", #str(args['data_ratio']),
+        "--shuffle_buffer_size",    "5000",
         # "--verbose",                "",
         "--delete_checkpoint",      "1",
     ])
@@ -156,21 +134,24 @@ def main(args):
 
     # prepare search space
     search_space  = [
-        Real(args.lr_min,               args.lr_max,            "log-uniform",  name='lr'),
-        Real(args.inv_mom_min,          args.inv_mom_max,       "log-uniform",  name='inv_mom'),
-        Real(args.lr_decay_min,         args.lr_decay_max,      "log-uniform",  name='lr_decay'),
-        Real(args.dropout_min,          args.dropout_max,       "uniform",      name='dropout'),
-        Real(args.l2_min,               args.l2_max,            "log-uniform",  name='l2'),
-        Real(args.alpha_min,            args.alpha_max,         "uniform",      name='alpha'),
-        Real(args.ce_ratio_min,         args.ce_ratio_max,      "uniform",      name='ce_ratio'),
-        Real(1e-5,                      100,                    "log-uniform",  name='loss_param_1'),
-        Integer(args.num_unfrozen_min,  args.num_unfrozen_max,                  name='num_unfrozen'),
-        # Integer(args.loss_param_1_min,  args.loss_param_1_max,                  name='loss_param_1'),
-        # Integer(args.loss_param_2_min,  args.loss_param_2_max,                  name='loss_param_2'),
-        # Integer(1,                      3,                                      name='data_ratio'),
-        # Integer(1,                      3,                                      name='batch_repeats'),
-        # Categorical(['all', 'knn', 'kfn', 'ksd'],                               name='con_filt_type'),
-        # Categorical(['all', 'knn', 'kfn', 'ksd'],                               name='pen_con_filt_type'),
+        Real(1e-06, 0.1,   "log-uniform",  name='lr'),
+        Real(0.01,  0.5,   "log-uniform",  name='inv_mom'),
+        Real(1e-07, 0.01,  "log-uniform",  name='lr_decay'),
+        Real(0.1,   0.8,   "uniform",      name='dropout'),
+        Real(1e-07, 0.001, "log-uniform",  name='l2'),
+        Real(0.1,   0.99,  "uniform",      name='alpha'),
+        Real(0,     1,     "uniform",      name='ce_ratio'),
+        Integer(16, 256,                   name='bs'),
+        Integer(0,  1,                     name='bn'),
+        Integer(0,  1,                     name='augment'),
+        # Real(1e-5, 100, "log-uniform",     name='loss_param_1'),
+        # Integer(0,  16,                    name='num_unfrozen'),
+        # Integer(1,  3,                     name='loss_param_1'),
+        # Integer(8,  128,                   name='loss_param_2'),
+        # Integer(1,   3,        name='data_ratio'),
+        # Integer(1,   3,        name='batch_repeats'),
+        # Categorical(['all', 'knn', 'kfn', 'ksd'],  name='con_filt_type'),
+        # Categorical(['all', 'knn', 'kfn', 'ksd'],  name='pen_con_filt_type'),
     ]
 
     if verbose:
