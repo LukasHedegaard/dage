d08e79b8690ca7289e00e86b15a7943ebd8637f7
Lukas Hedegaard <lh@eng.au.dk>
2020-02-17 09:30:44 +0100

    add angularlinear and l2norm layers from dsne code

--- layers/__init__.py
+++ layers/__init__.py
@@ -1,2 +1,4 @@
 from layers.pair import Pair
 from layers.dense_attention import DenseAttention
+from layers.angular_linear import AngularLinear 
+from layers.l2norm import L2NormInstance, l2norm_instance
