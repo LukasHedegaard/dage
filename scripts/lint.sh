e876da1c6a06384a97ba56575a6fa930af3bc2c0
LukasHedegaard <lh@eng.au.dk>
2021-04-12 12:20:47 +0000

    Fix linting script

--- scripts/lint.sh
+++ scripts/lint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 isort . --float-to-top --multi-line=3 --trailing-comma --force-grid-wrap=0 --use-parentheses --line-width=88	
-black -l 88 .	
+python -m black -l 88 .	
 flake8 . --max-complexity=12 --max-line-length=88 --select=C,E,F,W,B,B950,BLK --ignore=E203,E231,E501,W503 --exclude=.cache
